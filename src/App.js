b861654cb17b49cf4dedce3c0ad6788bceb21603
Jayme <jlm_developer@protonmail.com>
2022-01-10 19:51:31 -0600

    added text to App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import AutoCompleteTextField from './components/AutoCompleteTextField/AutoComple
 function App() {
   return (
     <div className="App">
-      Hello World. Enjoy these Common Custom React Components!
+      Hello World. Enjoy these Common Custom React Components! Just clone this repo and take the components you want to use and move them into the project you desire to use them in!
     </div>
   );
 }
